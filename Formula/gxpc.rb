7c1aca7bab6ca75e763c7e4ce86cd53b348b0b5c
Spotlight <spotlight@joscomputing.space>
2026-02-04 10:23:31 -0500

    gxpc: Require node

--- Formula/gxpc.rb
+++ Formula/gxpc.rb
@@ -6,6 +6,9 @@ class Gxpc < Formula
 
   depends_on :macos
 
+  # npm is required for building Frida modules at runtime.
+  depends_on "node"
+
   if Hardware::CPU.arm?
     url "https://github.com/ReverseApple/gxpc/releases/download/v2.2.1/gxpc_2.2.1_darwin_arm64.tar.gz"
     sha256 "3aa50f4c620f7132c37c6bb6fb6fb0f28a403f70983e5607db47c136555e90f7"
